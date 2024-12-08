1ebada91b088ec848833952fec5218376b8047dd
Ayanadiya <ayanadiya17@gmail.com>
2024-12-08 10:47:46 +0530

    seperated  javascript file and typescript file in directory

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -3,6 +3,9 @@ import { Request, Response, NextFunction } from 'express';
 
 import { Todo } from '../models/todos';
 
+type RequestBody={text:string};
+type RequestParams={todoId:string};
+
 let todos: Todo[]=[]
 
 const router=Router();
@@ -12,21 +15,24 @@ router.get('/', (req,res,next) => {
 });
 
 router.post('/todos', (req,res,next) => {
+    const body=req.body as RequestBody
     const newTodo: Todo ={
         id:new Date().toISOString(),
-        text:req.body.text
+        text:body.text
     };
     todos.push(newTodo);
     res.status(201).json({message:"Added Todo", todo:newTodo, todos:todos});
 })
 
 router.put('/todo/:todoId', (req:Request<{ todoId: string }>,res: Response,next:NextFunction) =>{
-    const tid= req.params.todoId;
+    const params=req.params as RequestParams
+    const tid= params.todoId;
+    const body =req.body as RequestBody
     const todoIndex=todos.findIndex(todoItem => todoItem.id===tid);
     if(todoIndex>=0){
         todos[todoIndex]={
             id:todos[todoIndex].id,
-            text:req.body.text};
+            text:body.text};
         res.status(200).json({message:'Updated todo', todos:todos});
         }
     else
@@ -36,7 +42,9 @@ router.put('/todo/:todoId', (req:Request<{ todoId: string }>,res: Response,next:
 });
 
 router.delete('/todo/:todoId', (req,res,next) =>{
-    todos=todos.filter((todoItem) => todoItem.id!==req.params.todoId);
+    const params=req.params as RequestParams
+    todos=todos.filter((todoItem) => todoItem.id!==params.todoId);
+    console.log(todos);
     res.status(200).json({message:'Deleted todo', todos:todos});
 });
 
